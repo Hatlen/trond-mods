ad175e5308aede2419a3896288e158760c0ca3a5
Trond Fröding Hatlen <trondfroding@gmail.com>
2018-12-21 16:48:12 +0100

    Add test file with class

---
+++ mods/translate-component-to-t-function/testFileClass.js
@@ -0,0 +1,31 @@
+import Translate from "components/translate";
+import { Component } from "react";
+
+class C extends Component {
+  render() {
+    const { totalVisitors, copied } = this.props;
+    return (
+      <div>
+        <Translate i18nKey="key" />
+        <Translate
+          count={totalVisitors}
+          totalViews={numeral(totalVisitors).format("0,0")}
+          i18nKey="key_with_pluralization"
+        />
+        <Translate
+          count={totalVisitors}
+          totalViews={numeral(totalVisitors).format("0,0")}
+          i18nKey="key_with_html"
+        />
+        <Translate
+          count={totalVisitors}
+          totalViews={numeral(totalVisitors).format("0,0")}
+          i18nKey="key_with_pluralization_and_html"
+        />
+        {copied ? <Translate i18nKey="key" /> : null}
+      </div>
+    );
+  }
+}
+
+export default C;
